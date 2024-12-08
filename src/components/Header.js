602b5448db7c0a9e6df947f2a2f72d15180fe7dd
pslavanya <lavanya2psai@gmail.com>
2024-12-07 23:41:20 -0600

    Updated logo for application

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,17 @@ import { Link } from 'react-router-dom';
 const Header = () => {
   return (
     <Navbar bg="dark" variant="dark" expand="lg" className="header-custom">
-      <Navbar.Brand href="/">LCS Private Limited</Navbar.Brand>
+      <Navbar.Brand href="/" className="d-flex align-items-center">
+        {/* Image path updated to be relative to the public folder */}
+        <img
+          src="/images/logo1.png"  // Path to the image in the public folder
+          alt="Company Logo"
+          width="21"  // You can adjust the width as needed
+          height="21"
+          className="mr-2"
+        />
+        LCS Private Limited
+      </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ml-auto">
